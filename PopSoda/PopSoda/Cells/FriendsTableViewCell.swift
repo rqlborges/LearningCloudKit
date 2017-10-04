cae239cac78103ecf2ff16abf10a65d85a2d2208
Erick Borges <ericklborges@gmail.com>
2017-10-04 14:05:09 -0300

    First commit

---
+++ PopSoda/PopSoda/Cells/FriendsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsTableViewCell.swift
+//  PopSoda
+//
+//  Created by Erick Borges on 04/10/2017.
+//  Copyright © 2017 Erick Borges. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PopSoda/PopSoda/Cells/MyDrinkTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyDrinkTableViewCell.swift
+//  PopSoda
+//
+//  Created by Erick Borges on 04/10/2017.
+//  Copyright © 2017 Erick Borges. All rights reserved.
+//
+
+import UIKit
+
+class MyDrinkTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
